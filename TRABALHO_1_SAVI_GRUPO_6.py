4a11051cbddddd1f4e0377d01d1d38f7dfa8a263
eduardop166 <eduardofilipepn@ua.pt>
2025-11-09 18:02:25 +0000

    Add files via upload
    TAREFA 1

--- TRABALHO_1_SAVI_GRUPO_6.py
+++ TRABALHO_1_SAVI_GRUPO_6.py
@@ -5,11 +5,8 @@ import os
 
 # Caminhos para as pastas 
 base_path = "tum_dataset"
-#rgb_path = r"C:\Users\Eduardo Pereira\Documents\UNI\A2-S1\SAVI\TRABALHO1\tum_dataset\rgb"
-#depth_path = r"C:\Users\Eduardo Pereira\Documents\UNI\A2-S1\SAVI\TRABALHO1\tum_dataset\depth"
-
-rgb_path = r"C:\Universidade\Mestrado\2o Ano\SAVI\savi-25-26-assignment1-group6\tum_dataset\rgb"
-depth_path = r"C:\Universidade\Mestrado\2o Ano\SAVI\savi-25-26-assignment1-group6\tum_dataset\depth"
+rgb_path = r"C:\Users\Eduardo Pereira\Documents\UNI\A2-S1\SAVI\TRABALHO1\tum_dataset\rgb"
+depth_path = r"C:\Users\Eduardo Pereira\Documents\UNI\A2-S1\SAVI\TRABALHO1\tum_dataset\depth"
 
 # Parâmetros intrínsecos da camera
 fx, fy = 525.0, 525.0
@@ -22,7 +19,7 @@ def process_rgbd_pair(rgb_file, depth_file):
     color_cv = cv2.cvtColor(cv2.imread(rgb_file), cv2.COLOR_BGR2RGB)
     depth_cv = cv2.imread(depth_file, cv2.IMREAD_UNCHANGED)
 
-    if depth_cv is None or color_cv is None:  
+    if depth_cv is None or color_cv is None:
         raise FileNotFoundError(f"Erro a ler {rgb_file} ou {depth_file}")
 
     # Garantir que esta em uint16
@@ -43,7 +40,7 @@ def process_rgbd_pair(rgb_file, depth_file):
     rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(
         color=color_raw,
         depth=depth_raw,
-        depth_scale=5000.0,   # padrão TUM, valores do depth vêm em milimetros 
+        depth_scale=5000.0,   # padrão TUM
         depth_trunc=4.0,      # corta profundidades > 4 m
         convert_rgb_to_intensity=False  # mantém cores reais
     )
@@ -76,5 +73,49 @@ pcd1 = process_rgbd_pair(os.path.join(rgb_path, "1.png"),
 pcd2 = process_rgbd_pair(os.path.join(rgb_path, "2.png"),
                          os.path.join(depth_path, "2.png"))
 
+
 # Visualização
-o3d.visualization.draw_geometries([pcd2])
+#o3d.visualization.draw_geometries([pcd2])
+
+############################################################
+#                TAREFA 1 — ICP com Open3D
+############################################################
+
+
+# definir parametros ICP
+
+threshold = 0.15  # distância máxima entre correspondências
+initial_transform = np.eye(4)  # transformação inicial identidade
+#initial_transform = np.asarray([[0.862, 0.011, -0.507, 0.5],
+#                         [-0.139, 0.967, -0.215, 0.7],
+#                         [0.487, 0.255, 0.835, -1.4],
+#                         [0.0, 0.0, 0.0, 1.0]])
+
+# Executar ICP
+
+# POINT TO PLANE
+icp_result = o3d.pipelines.registration.registration_icp(
+    source=pcd1,
+    target=pcd2,
+    max_correspondence_distance=threshold,
+    init=initial_transform,
+    estimation_method=o3d.pipelines.registration.TransformationEstimationPointToPlane())
+
+# POINT TO POINT
+#icp_result = o3d.pipelines.registration.registration_icp(
+#    source=pcd1,
+#    target=pcd2,
+#    max_correspondence_distance=threshold
+#    init=initial_transform,
+#    estimation_method=o3d.pipelines.registration.TransformationEstimationPointToPoint())
+
+# Aplica a transformacao e alinha
+
+pcd1_aligned = pcd1.transform(icp_result.transformation)
+
+# visuzlizar
+
+pcd1.paint_uniform_color([1, 0, 0])  # vermelho = fonte transformada
+pcd2.paint_uniform_color([0, 0, 1])  # azul = alvo
+
+o3d.visualization.draw_geometries([pcd1, pcd2])
